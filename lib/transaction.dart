7c2af818cba2b3abe3a4d9bdc6f7835c04200cd1
Leonardo Amato <leo.amato@hotmail.com>
2022-09-27 18:38:33 +0200

    first commit

---
+++ lib/transaction.dart
@@ -0,0 +1,13 @@
+class Transaction {
+  String id;
+  String title;
+  double amount;
+  DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.amount,
+    required this.date,
+    required this.title,
+  });
+}
